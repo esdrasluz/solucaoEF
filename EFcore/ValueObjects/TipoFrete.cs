1baee69533f0f88d961a279b0d6b9a5d2ace1072
esdrasluz <84642368+esdrasluz@users.noreply.github.com>
2023-05-29 23:27:48 -0300

    criando um solução com Entity Framework Core
    fazendo uma solução de Entity Framework Core numa aplicação de sistema de pedidos com atribuição, inserindo e removendo pedidos.

---
+++ EFcore/ValueObjects/TipoFrete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcore.ValueObjects
+{
+     class enum TipoFrete
+    {
+        CPF,
+        FOB,
+
+
+    }
+}
